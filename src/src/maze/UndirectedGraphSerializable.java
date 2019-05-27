db220cf5fc5ff587b88b721725be8ef91e746920
GuillaumeClementPolytech <43247385+GuillaumeClementPolytech@users.noreply.github.com>
2019-05-27 16:35:33 +0200

    Add files via upload

---
+++ src/src/maze/UndirectedGraphSerializable.java
@@ -0,0 +1,11 @@
+package maze;
+
+import impl.*;
+
+public class UndirectedGraphSerializable extends UndirectedGraph implements java.io.Serializable { 
+
+	public UndirectedGraphSerializable(int a, int b){
+		super(a,b);
+	}
+
+} 
