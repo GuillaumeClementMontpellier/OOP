4c20664abe7964f739768b93edcd478aff0ae0b3
GuillaumeClementPolytech <43247385+GuillaumeClementPolytech@users.noreply.github.com>
2019-05-27 16:36:42 +0200

    Add files via upload

---
+++ src/src/maze/p4/src/maze/Game.java
@@ -0,0 +1,101 @@
+package maze;
+
+import java.io.*;
+import java.util.*;
+
+public class Game {
+
+	public static void main (String[] args){
+
+		//init I/O
+
+		Scanner lineTokenizer;
+
+		String inputLine = "1";
+		Console inputReader = System.console();
+
+		if(inputReader == null){
+			System.err.println("No Console");
+			System.exit(1);
+		}
+
+		//input user pour les 2 cas
+
+		inputLine = inputReader.readLine();
+
+		//traitement input
+
+		if (inputLine.equals("1")){
+
+			Maze m = new Maze(3, 3, 0, 0, 2, 2);
+
+			System.out.println("Yay !");
+
+			m.setVide(0,1);
+			m.setVide(0,2);
+			m.setVide(1,2);
+
+			m.setVide(1,0);
+			m.setVide(2,0);
+			m.setVide(2,1);
+
+			System.out.println(m);
+
+		} else {
+
+			Maze m = new Maze(3, 3, 0, 0, 2, 2);
+
+			m.setVide(0,1);
+			m.setVide(0,2);
+			m.setVide(1,2);
+
+			m.setVide(1,0);
+			m.setVide(2,0);
+			m.setVide(2,1);
+
+			//charge sauve.maze 
+
+			try{
+				FileInputStream sauvegarde = new FileInputStream("save.maze");
+				ObjectInputStream in = new ObjectInputStream(sauvegarde);
+				m = (Maze) in.readObject();
+
+				in.close();
+				sauvegarde.close();
+
+			} catch (IOException i){
+				System.out.println("NOOOOOOOOOOOO");
+
+				i.printStackTrace();
+				System.exit(1);				
+			} catch (ClassNotFoundException c){
+				c.printStackTrace();
+				System.exit(1);		
+			}
+
+			//
+
+
+			//sauvegarde le maze
+			try {
+
+				FileOutputStream sauvegarde = new FileOutputStream("save.maze");
+				ObjectOutputStream out = new ObjectOutputStream(sauvegarde);
+
+				out.writeObject(m);
+				out.close();
+				sauvegarde.close();
+
+				System.out.println("Yes !!!");
+
+
+			} catch (IOException e){
+				e.printStackTrace();
+				System.err.println(e.getMessage());
+				System.exit(1);
+			}
+
+		}
+
+	}
+}
---
+++ src/src/maze/p4/src/maze/UndirectedEdgeSerializable.java
@@ -0,0 +1,13 @@
+package maze;
+
+import graph.*;
+
+public class UndirectedEdgeSerializable extends UndirectedEdge implements java.io.Serializable { 
+
+	public UndirectedEdgeSerializable(Vertex x, Vertex y){
+
+		super(x,y);
+
+	}
+
+} 
